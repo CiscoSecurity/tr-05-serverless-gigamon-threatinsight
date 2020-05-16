d1410172c6227489bccbe80efa0f4d3e5e4e26e3
Gevorg Davoian <gdavoian@gmail.com>
2020-05-16 12:18:04 +0300

    Add some event-specific details and extra relations to sightings

--- api/mappings.py
+++ api/mappings.py
@@ -1,4 +1,5 @@
 import abc
+from collections import namedtuple
 from typing import Dict, Any
 from uuid import uuid4
 
@@ -19,6 +20,9 @@ class Mapping(abc.ABC):
 CTIM_DEFAULTS = {
     'schema_version': '1.0.16',
 }
+
+
+Observable = namedtuple('Observable', ('type', 'value'))
 
 
 class Sighting(Mapping):
@@ -45,19 +49,7 @@ class Sighting(Mapping):
 
         sighting['observed_time'] = {'start_time': event['timestamp']}
 
-        if 'detection' in event:
-            sighting['data'] = {
-                'columns': [
-                    {'name': 'Impacted Devices', 'type': 'integer'},
-                    {'name': 'Indicator Values', 'type': 'integer'},
-                ],
-                'rows': [
-                    [
-                        event['detection']['summary']['impacted_devices'],
-                        event['detection']['summary']['indicator_values'],
-                    ],
-                ]
-            }
+        sighting['data'] = cls._details(event)
 
         sighting['description'] = '\n'.join(
             [f'- Event: `{event["event_type"].upper()}`.'] + (
@@ -97,28 +89,172 @@ class Sighting(Mapping):
         return sighting
 
     @staticmethod
+    def _details(event):
+        columns = []
+        rows = []
+
+        if event['event_type'] == 'flow':
+            columns.extend([
+                {'name': 'flow_state', 'type': 'string'},
+                {'name': 'proto', 'type': 'string'},
+                {'name': 'service', 'type': 'string'},
+                {'name': 'total_pkts', 'type': 'integer'},
+            ])
+            rows.append([
+                event['flow_state'],
+                event['proto'],
+                event['service'],
+                event['total_pkts'],
+            ])
+
+        if event['event_type'] == 'dns':
+            columns.extend([
+                {'name': 'qtype', 'type': 'integer'},
+                {'name': 'qtype_name', 'type': 'string'},
+                {'name': 'rcode', 'type': 'integer'},
+                {'name': 'rcode_name', 'type': 'string'},
+                # Use 'string' instead of 'boolean' (not supported yet).
+                {'name': 'rejected', 'type': 'string'},
+            ])
+            rows.append([
+                event['qtype'],
+                event['qtype_name'],
+                event['rcode'],
+                event['rcode_name'],
+                # False -> 'false', True -> 'true'.
+                str(event['rejected']).lower(),
+            ])
+
+        if event['event_type'] == 'http':
+            columns.extend([
+                {'name': 'method', 'type': 'string'},
+                {'name': 'status_code', 'type': 'integer'},
+                {'name': 'status_msg', 'type': 'string'},
+                {'name': 'files', 'type': 'integer'},
+            ])
+            rows.append([
+                event['method'],
+                event['status_code'],
+                event['status_msg'],
+                len(event['files'] or []),
+            ])
+
+        if event['event_type'] == 'ssh':
+            columns.extend([
+                {'name': 'direction', 'type': 'string'},
+                {'name': 'client', 'type': 'string'},
+                {'name': 'server', 'type': 'string'},
+            ])
+            rows.append([
+                event['direction'],
+                event['client'],
+                event['server'],
+            ])
+
+        if event['event_type'] == 'suricata':
+            columns.extend([
+                {'name': 'sig_name', 'type': 'string'},
+                {'name': 'sig_category', 'type': 'string'},
+                {'name': 'sig_id', 'type': 'integer'},
+                {'name': 'sig_rev', 'type': 'number'},
+            ])
+            rows.append([
+                event['sig_name'],
+                event['sig_category'],
+                event['sig_id'],
+                event['sig_rev'],
+            ])
+
+        if columns:
+            # Add a bullet before each column (i.e. field) name for better
+            # appearance on the UI.
+            for column in columns:
+                column['name'] = '•' + ' ' + column['name']
+
+            # Add some "header" column for the event-specific details.
+            columns.insert(0, {'name': 'Event Summary', 'type': 'string'})
+            rows[0].insert(0, ' ')
+
+        if 'detection' in event:
+            # Make the detection-specific details come before the
+            # event-specific ones to visually highlight the former from the
+            # latter on the UI.
+            # Add some "header" column for the detection-specific details.
+            extra_columns = [
+                {'name': 'Detection Summary', 'type': 'string'},
+                {'name': 'Impacted Devices', 'type': 'integer'},
+                {'name': 'Indicator Values', 'type': 'integer'},
+            ]
+            extra_row = [
+                ' ',
+                event['detection']['summary']['impacted_devices'],
+                event['detection']['summary']['indicator_values'],
+            ]
+
+            columns = extra_columns + columns
+            if rows:
+                for index, row in enumerate(rows):
+                    rows[index] = extra_row + row
+            else:
+                rows.append(extra_row)
+
+        return {
+            'columns': columns,
+            'rows': rows,
+        }
+
+    @staticmethod
     def _relations(origin, event):
         relations = []
 
-        for source_obj, related_obj, relation in [
-            ('src', 'dst', 'Connected_To'),
-            ('dst', 'src', 'Connected_From'),
-        ]:
-            if event[source_obj]['internal']:
-                relations.append({
-                    'origin': origin,
-                    'related': {
-                        'type': 'ip',
-                        'value': event[related_obj]['ip'],
-                    },
-                    'relation': relation,
-                    'source': {
-                        'type': 'ip',
-                        'value': event[source_obj]['ip'],
-                    },
-                })
+        def append_relation(
+            source: Observable,
+            relation: str,
+            related: Observable,
+        ) -> None:
+            relations.append({
+                'origin': origin,
+                'related': {
+                    'type': related.type,
+                    'value': related.value,
+                },
+                'relation': relation,
+                'source': {
+                    'type': source.type,
+                    'value': source.value,
+                }
+            })
+
+        append_relation(
+            Observable('ip', event['src']['ip']),
+            'Connected_To',
+            Observable('ip', event['dst']['ip']),
+        )
 
         # TODO: come up with more possible relations of interest
+
+        if event['event_type'] == 'dns':
+            append_relation(
+                Observable('ip', event['src']['ip']),
+                'Queried',
+                Observable('domain', event['query']['domain'])
+            )
+
+            if event['answers']:
+                for answer in event['answers']:
+                    append_relation(
+                        Observable('domain', event['query']['domain']),
+                        'Resolved_To',
+                        Observable('ip', answer['ip'])
+                    )
+
+        if event['event_type'] == 'http':
+            if event['user_agent']:
+                append_relation(
+                    Observable('user_agent', event['user_agent']),
+                    'Sent_By',
+                    Observable('ip', event['src']['ip']),
+                )
 
         return relations
 
