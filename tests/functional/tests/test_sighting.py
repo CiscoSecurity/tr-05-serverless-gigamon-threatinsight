1dce3c0f68c93dc8a8b5a3280ad2abedb9c52c5f
Pavlo Havrylov <greenpasha22@gmail.com>
2020-05-26 14:44:23 +0300

    improve checks for sighting

--- tests/functional/tests/test_sighting.py
+++ tests/functional/tests/test_sighting.py
@@ -37,6 +37,11 @@ def test_positive_sighting(module_headers, observable, observable_type):
     sightings = get_observables(
         response, 'Gigamon ThreatINSIGHT')['data']['sightings']
     confidence_levels = ['High', 'Info', 'Low', 'Medium', 'None', 'Unknown']
+    relations_type = [
+        'Connected_To', 'Sent_From', 'Sent_To',
+        'Resolved_To', 'Hosted_On', 'Queried_For'
+    ]
+    targets_observables_types = ['ip', 'hostname', 'mac_address']
 
     assert len(sightings['docs']) > 0
 
@@ -44,17 +49,38 @@ def test_positive_sighting(module_headers, observable, observable_type):
         assert sighting['description']
         assert sighting['confidence'] in confidence_levels
         assert sighting['count'] == 1
-        assert sighting['id']
+        assert sighting['id'].startswith('transient:')
         assert sighting['observed_time']['start_time']
         assert sighting['schema_version']
         assert sighting['observables'][0] == observables[0]
         assert sighting['external_ids']
         assert sighting['type'] == 'sighting'
         assert sighting['source'] == 'Gigamon ThreatINSIGHT'
+        assert sighting['source_uri'].startswith('https://portal.icebrg.io/')
+
+        for external_reference in sighting['external_references']:
+            assert external_reference['external_id'] in (
+                   sighting['external_ids'])
+            assert external_reference['source_name'] == sighting['source']
+            assert external_reference['description']
+            assert external_reference['url']
+
+        for relation in sighting['relations']:
+            assert relation['origin'] == 'Gigamon ThreatINSIGHT'
+            assert relation['relation'] in relations_type
+            assert relation['source']['value']
+            assert relation['source']['type']
+            assert relation['related']['value']
+            assert relation['related']['type']
+            if relation['relation'] == 'Hosted_On':
+                assert relation['source']['value'].startswith('http://') and (
+                       relation['source']['type'] == 'url')
+
         assert sighting['sensor']
-        assert sighting['internal']
+        assert sighting['internal'] is True
         assert sighting['targets'][0]['type'] == 'endpoint'
         assert sighting['targets'][0]['observed_time']['start_time']
-        assert sighting['targets'][0]['observables'][0]['type'] == 'ip'
+        for observable_type in sighting['targets'][0]['observables']:
+            assert observable_type['type'] in targets_observables_types
 
     assert sightings['count'] == len(sightings['docs'])
