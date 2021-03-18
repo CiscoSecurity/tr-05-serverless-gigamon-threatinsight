ff203e1d9aff2265f53a68dba554854f5582f559
ytsek <79151889+ytsek@users.noreply.github.com>
2021-03-18 15:34:20 +0200

    Restructure code (#41)
    * Some restructure
    
    * Delete test_requirements and travis_key
    
    * Add watchdog add, requirements fix
    
    * Scripts add
    
    * Watchdog errorhandler add
    
    * Module type add
    
    * Serverless and relay marks change
    
    * Object Replacement Characters removed
    
    * Add test_watchdog and minor changes in module_type
    
    * Module type fix

--- code/config.py
+++ code/config.py
@@ -1,8 +1,9 @@
-from __version__ import VERSION
+import json
 
 
 class Config:
-    VERSION = VERSION
+    settings = json.load(open('container_settings.json', 'r'))
+    VERSION = settings["VERSION"]
 
     CTR_USER_AGENT = (
         'SecureX Threat Response Integrations '
